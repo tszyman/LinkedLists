d54080f9bf75421c0033886e7e743018860acaa4
Tomasz Szymanski <tszymans@student.42.fr>
2024-08-26 20:43:18 +0200

    Fisrt iterations of lists

--- lists.c
+++ lists.c
@@ -6,7 +6,7 @@
 /*   By: tszymans <tszymans@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/26 20:22:09 by tszymans          #+#    #+#             */
-/*   Updated: 2024/08/26 20:35:43 by tszymans         ###   ########.fr       */
+/*   Updated: 2024/08/26 20:42:43 by tszymans         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,18 @@ int	main(int argc, char **argv)
 {
 	t_node	root;
 	t_node	elem2;
+	t_node	*curr;
 
 	root.x = 15;
 	root.next = malloc(sizeof(t_node));
 	root.next->x = 29;
 	root.next->next = NULL;
-
-	printf("First element: %d\n", root.x);
-	printf("Second element: %d\n", root.next->x);
+	curr = &root;
+	while (curr != NULL)
+	{
+		printf("%d\n", curr->x);
+		curr = curr->next;
+	}
 
 	free(root.next);
 	return (0);
